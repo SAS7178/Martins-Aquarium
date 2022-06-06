060b8d102db6ad890ed326af2d9ee77eac7741fd
Stephen Smith <StephenASmith7178@gmail.com>
2022-06-06 14:16:48 -0500

    fix to locations align

--- scripts/locations.js
+++ scripts/locations.js
@@ -11,9 +11,7 @@ export const locationsList = () => {
         HTMLString += `<section class= "location_Card">
         <div class= "location_quote">${location.qoute}</div>
         <div><img  class="location__image image--card" src="${location.locationImage}" /></div>
-        <div>${location.location}</div>
-        <section/>
-        `
+        <div class= "location_name">${location.location}</div>        `
     }
     HTMLString += `</article>`
 
